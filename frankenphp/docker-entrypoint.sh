eb7945769d178d5d6e9bf1dd543fd2ef331c0883
vics80 <victorvgm@gmail.com>
2024-11-06 10:59:32 +0100

    Update docker-entrypoint.sh

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -16,10 +16,10 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		composer require "php:>=$PHP_VERSION" runtime/frankenphp-symfony
 		composer config --json extra.symfony.docker 'true'
 
-		if grep -q ^DATABASE_URL= .env; then
-			echo "To finish the installation please press Ctrl+C to stop Docker Compose and run: docker compose up --build -d --wait"
-			sleep infinity
-		fi
+		#if grep -q ^DATABASE_URL= .env; then
+		#	echo "To finish the installation please press Ctrl+C to stop Docker Compose and run: docker compose up --build -d --wait"
+		#	sleep infinity
+		#fi
 	fi
 
 	if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
