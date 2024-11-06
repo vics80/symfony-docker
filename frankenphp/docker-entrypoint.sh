9e69e1869c18e0b3446b1b0c2169c58c8ddc6c69
vics80 <victorvgm@gmail.com>
2024-11-06 11:44:42 +0100

    Update docker-entrypoint.sh

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -15,6 +15,7 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
 		composer require "php:>=$PHP_VERSION" runtime/frankenphp-symfony
 		composer config --json extra.symfony.docker 'true'
+  		composer require symfony/orm-pack
 
 		#if grep -q ^DATABASE_URL= .env; then
 		#	echo "To finish the installation please press Ctrl+C to stop Docker Compose and run: docker compose up --build -d --wait"
